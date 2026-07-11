3bacb814b3b2a9723217542889fb5c9a2986d927
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-07-11 23:09:45 +0500

    fix: update static file serving and change root route to render home view

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = 3000;
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join('/app', "public", "index.html"));
+    res.render("user/home");
 });
 
 app.listen(PORT, () => {console.log("server is listening at port: ", PORT)});
