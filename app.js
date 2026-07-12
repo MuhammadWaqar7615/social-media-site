0af0358f1a1438cf39cf7832b2e3356b3974e296
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-07-12 13:09:32 +0500

    fix: update route handling and restore static file serving

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = 3000;
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("user/home");
-});
+app.use(express.static(path.join(__dirname, "public")));
+app.get('/', (req, res) => res.send('Server is working!'))
 
 app.listen(PORT, () => {console.log("server is listening at port: ", PORT)});
