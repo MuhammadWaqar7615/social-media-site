dd840f336e2f6f12c815cb756fcce9b519e8974b
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-07-15 23:35:09 +0500

    fix: add postRoutes middleware and update root route response

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const postRouters = require('./routes/postRoutes');
 const path = require('path')
 const app = express()
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
-app.get('/', (req, res) => res.send('Server is working!'))
+
+app.use(postRouters);
 
 app.listen(PORT, () => {console.log("server is listening at port: ", PORT)});
