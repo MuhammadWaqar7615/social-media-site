5e284e877b067bebeba1d3c527d85edd8368b42e
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-07-16 15:34:32 +0500

    feat: enhance post creation and feed pages with navigation and improved layout

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getPosts, createNewPost, getFeedPage, getCreatePage } = require('../controllers/postController');
+const { getPosts, createNewPost, getFeedPage, getCreatePage, deletePost } = require('../controllers/postController');
 
 router.get('/', getFeedPage);
 router.get('/create', getCreatePage);
 router.get('/api/posts', getPosts);
 router.post('/api/posts', createNewPost);
+router.delete('/api/post/:id', deletePost);
 
 module.exports = router;
