9f7cef0cb60600a016ea3187677f501a1c90ba8b
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-07-19 23:03:08 +0500

    fix: clean up postController.js by removing commented code and improving async handling

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,3 @@
-// const postModel = require('../models/postModel');
 const { getAllPosts, addNewPost, handledlete } = require('../models/postModel');
 const path = require('path');
 
@@ -9,27 +8,27 @@ const getFeedPage = ((req, res) => {
 const getCreatePage = ((req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'create.html'));
 })
+
 const getPosts = async (req, res) => {
     const posts = await getAllPosts();
-    // console.log('These are the total posts: ', posts);
-    const resp = res.json({posts})
+    res.json({posts});
 }
 
 const createNewPost = async (req, res) => {
-    // console.log("req", req.body);
     const name = req.body.username;
     const content = req.body.content;
-    const createPost = await addNewPost(name, content);
-    res.redirect('/')
+    await addNewPost(name, content);
+    res.redirect('/feed');
 }
-const deletePost = ((req, res) => {
+
+const deletePost = async (req, res) => {
     const reqId = req.params.id;
-    handledlete(reqId);
+    await handledlete(reqId);
     res.status(200).json({
         message: 'Post deleted successfully',
         id: reqId
-    })
-})
+    });
+}
 
 module.exports = {
     getFeedPage,
